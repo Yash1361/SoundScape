7e10e1f08f1a5fce42e682a68995c17d9ff95e30
Yash Aggarwal <95673884+Yash1361@users.noreply.github.com>
2024-09-15 10:59:59 -0400

    ..

---
+++ NewViewUITests/NewViewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NewViewUITestsLaunchTests.swift
+//  NewViewUITests
+//
+//  Created by Yash Aggarwal on 14/09/24.
+//
+
+import XCTest
+
+final class NewViewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
